6e4ab01d33324daa58ca14ad163a06a83bf3e60f
Inesh0508 <inesh.ag05@gmail.com>
2026-05-31 12:34:51 +0530

    added dat.gui

--- Three.js-TypeScript-Boilerplate/src/main.ts
+++ Three.js-TypeScript-Boilerplate/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -30,11 +31,34 @@ const stats = new Stats()
 // stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom)
 
+const gui = new GUI()
+
+const cubePosition = gui.addFolder('Cube_position')
+cubePosition.add(cube.position, 'x', -5, 5).name('Position X')
+cubePosition.add(cube.position, 'y', -5, 5).name('Position Y')
+cubePosition.add(cube.position, 'z', -5, 5).name('Position Z')
+const cubeRotation = gui.addFolder('cube_rotation')
+cubeRotation.add(cube.rotation, 'x', 0, Math.PI * 2).name('Rotation X')
+cubeRotation.add(cube.rotation, 'y', 0, Math.PI * 2).name('Rotation Y')
+cubeRotation.add(cube.rotation, 'z', 0, Math.PI * 2).name('Rotation Z')
+const cameraPosition = gui.addFolder('Camera_position')
+cameraPosition.add(camera.position, 'x', -10, 10).name('Position X')
+cameraPosition.add(camera.position, 'y', -10, 10).name('Position Y')
+cameraPosition.add(camera.position, 'z', -10, 10).name('Position Z')
+const cameraRotation = gui.addFolder('Camera_rotation')
+cameraRotation.add(camera.rotation, 'x', 0, Math.PI * 2).name('Rotation X')
+cameraRotation.add(camera.rotation, 'y', 0, Math.PI * 2).name('Rotation Y')
+cameraRotation.add(camera.rotation, 'z', 0, Math.PI * 2).name('Rotation Z')
+cameraRotation.open()
+cameraPosition.open()
+cubePosition.open()
+cubeRotation.open()
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
 
