6923bcf30b746c57ea96d943bf79f96b0c587719
Inesh0508 <inesh.ag05@gmail.com>
2026-06-01 09:20:08 +0530

    added basic header

--- Three.js-TypeScript-Boilerplate/src/main.ts
+++ Three.js-TypeScript-Boilerplate/src/main.ts
@@ -5,25 +5,30 @@ import * as THREE from 'three'
 // import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x112266)
+scene.background = new THREE.Color(0x001a8c)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const main = document.getElementById('main') as HTMLCanvasElement
+const mainRenderer = new THREE.WebGLRenderer({ canvas: main })
+mainRenderer.setSize(window.innerWidth, 0.9*window.innerHeight)
+document.body.appendChild(mainRenderer.domElement)
+
+const header = document.getElementById('header') as HTMLCanvasElement
+const headerRenderer = new THREE.WebGLRenderer({ canvas: header })
+headerRenderer.setSize(window.innerWidth, 0.1*window.innerHeight)
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  mainRenderer.setSize(window.innerWidth, 0.9*window.innerHeight)
 })
 
 function animate() {
   requestAnimationFrame(animate)
 
-  renderer.render(scene, camera)
+  mainRenderer.render(scene, camera)
 }
 
 animate()
