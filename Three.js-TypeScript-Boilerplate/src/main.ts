70e5668a9501007ccfeb85cafb716d2fef98f777
Inesh0508 <inesh.ag05@gmail.com>
2026-06-01 09:11:07 +0530

    created the basic web page

--- Three.js-TypeScript-Boilerplate/src/main.ts
+++ Three.js-TypeScript-Boilerplate/src/main.ts
@@ -1,5 +1,29 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'dat.gui'
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+// import Stats from 'three/addons/libs/stats.module.js'
+// import { GUI } from 'dat.gui'
+
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x112266)
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  renderer.render(scene, camera)
+}
+
+animate()
